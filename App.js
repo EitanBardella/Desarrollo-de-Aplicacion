e1f8d3eaee671e13e310eb016694c154138ad048
Eitan <xxeitanxx11@gmail.com>
2024-01-18 21:04:10 -0300

    Se agrega el proyecto

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import img from "./assets/favicon.png"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Esta es mi App!!</Text>
+      <Image source={img} style={{ width: 100, height: 100, marginTop:50 }} />
       <StatusBar style="auto" />
     </View>
   );
